af5cd4cbf9ed682f2a9c802aec6cc66c5f3cae2e
malengatiger <malengatiger@gmail.com>
2021-03-11 05:49:24 +0200

    🍎  initial commit II

---
+++ android/app/src/main/java/com/boha/money_admin_2021/MainActivity.java
@@ -0,0 +1,6 @@
+package com.boha.money_admin_2021;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:money_admin_2021/ui/intro/intro_main.dart';
+import 'package:money_library_2021/util/util.dart';
+
+void main() async {
+  runApp(MyApp());
+  await Firebase.initializeApp();
+  p('🔵 🔵 🔵 Firebase initialized ....');
+  await DotEnv.load(fileName: '.env');
+  p('🍎 🍎 🍎 DotEnv has been loaded - this is so cool! ');
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: IntroMain(),
+    );
+  }
+}
